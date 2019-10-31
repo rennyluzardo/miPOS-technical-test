d4d36481de0684f9f05d8988fd188e36f8229936
Renny <rennyluzardo.fullstack@gmail.com>
2019-10-31 04:57:40 -0500

    fixes generals in main layout

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../../scss/styles.scss'
 
-export default class MainLayout extends Component {
-    render() {
-        return (
-            <div className="top__container">
-                <div className="top__container__titles-box">
-                    <div className="top__container__titles-box--left">Apertura</div>
-                    <div className="top__container__titles-box--right">Cierre</div>
-                </div>
-                <div className="top__container__content-box">{ this.props.children }</div>
+const MainLayout = props => {
+    return (
+        <div className="top__container">
+            <div className="top__container__titles-box">
+                <div className="top__container__titles-box--left">{props.leftTitle}</div>
+                <div className="top__container__titles-box--right">{props.rightTitle}</div>
             </div>
-        )
-    }
+            <div className="top__container__content-box">{props.children}</div>
+        </div>
+    )
 }
+
+export default MainLayout
